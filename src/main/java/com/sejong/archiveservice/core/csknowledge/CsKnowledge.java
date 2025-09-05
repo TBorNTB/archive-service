f1f9daa796a3bb9cf39d35f700ad0b332c1197d1
PlusUltraCode <kkd06144@naver.com>
2025-09-05 16:09:41 +0900

    #8 feat: CsKnowledge 엔티티 및 매핑 로직 수정

--- src/main/java/com/sejong/archiveservice/core/csknowledge/CsKnowledge.java
+++ src/main/java/com/sejong/archiveservice/core/csknowledge/CsKnowledge.java
@@ -1,6 +1,10 @@
 package com.sejong.archiveservice.core.csknowledge;
 
 import java.time.LocalDateTime;
+
+import com.sejong.archiveservice.application.exception.BaseException;
+import com.sejong.archiveservice.application.exception.ExceptionType;
+import com.sejong.archiveservice.core.user.UserId;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +19,13 @@ public class CsKnowledge {
     private Long id;
     private String title;
     private String content;
+    private UserId writerId;
     private TechCategory category;
     private LocalDateTime createdAt;
+
+    public void validateOwnerPermission(String username) {
+        if(!writerId.userId().equals(username)){
+            throw new BaseException(ExceptionType.FORBIDDEN);
+        }
+    }
 }
